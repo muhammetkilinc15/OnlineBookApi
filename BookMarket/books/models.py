d1d9676f6cf014160498a3ef2827433110864240
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-28 17:20:48 +0300

    Added author model

--- BookMarket/books/models.py
+++ BookMarket/books/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from datetime import date, datetime  # datetime modülünü import ettik
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=55)
+    surname = models.CharField(max_length=55)
+    birthDate = models.DateField()
+
+    @property
+    def age(self):
+        today = date.today()
+        return today.year - self.birthDate.year - ((today.month, today.day) < (self.birthDate.month, self.birthDate.day))
+
+    def __str__(self):
+        return f"{self.name} - {self.surname}"
+    
+        
+    
 class Book(models.Model):
     name = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books',null=True)
     description = models.TextField(blank=True,null=True,max_length=3000)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -13,6 +29,7 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.name} -  {self.author}"
     
+
 
 
 class Comment(models.Model):
@@ -26,5 +43,5 @@ class Comment(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],)
     
     def __str__(self):
-        return f"{self.commenter} - {self.rating}"
+        return f"{self.commenter} - {self.book.name} {self.rating}"
     
