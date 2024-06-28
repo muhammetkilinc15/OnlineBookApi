33a62ddca44bd8e8b54a418877323233b1998b9c
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-28 17:20:31 +0300

    Added AuthorAdmin

--- BookMarket/books/admin.py
+++ BookMarket/books/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from books.models import Book,Comment
+from books.models import Book,Comment,Author
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'birthDate', 'age')  # age alanını listeye ekledik
+
+
 
 admin.site.register(Book)
 admin.site.register(Comment)
+admin.site.register(Author, AuthorAdmin)
