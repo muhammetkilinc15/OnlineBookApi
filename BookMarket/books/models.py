11721151e4cbb40f5d4af27f1f9a543983e5eb64
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-28 15:47:46 +0300

    Added books and comment model

--- BookMarket/books/models.py
+++ BookMarket/books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True,max_length=3000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} -  {self.author}"
+    
+
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book,on_delete=models.CASCADE,related_name="comments")
+    commenter = models.CharField(max_length=255)
+    comment = models.TextField(blank=True,null=True,max_length=3000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],)
+    
+    def __str__(self):
+        return self.rating
+    
