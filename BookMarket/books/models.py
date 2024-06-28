1e96f39f7f33a65406c85361309592a285c28ae4
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-28 16:38:43 +0300

    Created Book and Comment Model

--- BookMarket/books/models.py
+++ BookMarket/books/models.py
@@ -26,5 +26,5 @@ class Comment(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],)
     
     def __str__(self):
-        return self.rating
+        return f"{self.commenter} - {self.rating}"
     
