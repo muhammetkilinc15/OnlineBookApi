9291f1ee864f6bb58953dea88873c1dfca1c894a
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-29 16:12:46 +0300

    Added Author list and detail url

--- BookMarket/books/api/urls.py
+++ BookMarket/books/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from books.api import views as api_views
 urlpatterns = [
-    path('books/',api_views.BookListCreateAPIView.as_view(),name="book-list")
+    path('books/',api_views.BookListCreateAPIView.as_view(),name="book-list"),
+    path('books/<int:pk>',api_views.BookDetailAPIView.as_view(),name="book-detail"),
+      
+    path('author/',api_views.AuthorListCreateAPIView.as_view(),name="author-list"),
+    path('author/<int:pk>',api_views.AuthorDetailAPIView.as_view(),name="author-detail")
 ]
