9ab9c4fcc87192efe0ed9c85e983180930b56b23
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-30 13:37:14 +0300

    User can make comment and access the comment details

--- BookMarket/books/api/urls.py
+++ BookMarket/books/api/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('books/<int:pk>',api_views.BookDetailAPIView.as_view(),name="book-detail"),
       
     path('author/',api_views.AuthorListCreateAPIView.as_view(),name="author-list"),
-    path('author/<int:pk>',api_views.AuthorDetailAPIView.as_view(),name="author-detail")
+    path('author/<int:pk>',api_views.AuthorDetailAPIView.as_view(),name="author-detail"),
+    
+     path('books/<int:book_pk>/makecomment/', api_views.CommentCreateAPIView.as_view(), name='make-comment'),
+     path('comments/<int:pk>', api_views.CommentDetailAPIView.as_view(), name='comments'),
 ]
