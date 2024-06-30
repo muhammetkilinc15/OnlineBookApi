1c5c60478e7e427659fa53fe9e8a2b19a539985c
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-06-30 13:36:37 +0300

    Created Comment Serializer

--- BookMarket/books/api/serializers.py
+++ BookMarket/books/api/serializers.py
@@ -2,23 +2,26 @@ from rest_framework import serializers
 from books.models import Book,Comment,Author
 
 
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
-    
-    
-    
+        #fields = '__all__'
+        exclude = ('book',) # kitap dısındakiler gelecek
+ 
+ 
+
+ 
+
+     
 # read-only diyerek bir kitap girilecen de yorum girilmesini beklemicek   
 class BookSerializer(serializers.ModelSerializer):
-    #allComments = CommentSerializer(many=True,read_only=True)
+    comments = CommentSerializer(many=True,read_only=True)
     class Meta:
         model = Book
-        fields = ['id', 'name', 'description', 'created_date', 'updated_date', 'published_date']
+        fields = ['id','comments', 'name', 'description', 'created_date', 'updated_date', 'published_date',]
         
 
+    
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)  # Use 'books' to match the related_name in the Book model
 
